32900418693795fdc80add0cea376acaf3f8b4da
vijay-navarkar <vijay.a.navarkar@gmail.com>
2020-02-09 08:19:21 +0530

    added first commit fir eureka server

---
+++ src/main/java/com/fdx/rtct/eurekaRegistry/EurekaRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.fdx.rtct.eurekaRegistry;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRegistryApplication.class, args);
+	}
+
+}
